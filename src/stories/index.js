741c64c5ee6548061cdc508dde7bf81a4d78339d
Pierre <dumaypierre@outlook.fr>
2018-05-22 10:32:04 +0200

    delete old readme

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,14 +6,16 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+//
+// ────────────────────────────────────────────────────── I ──────────
+//   :::::: E X E M P L E : :  :   :    :     :        :          :
+// ────────────────────────────────────────────────────────────────
+//
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+/* storiesOf('Bienvenue', module).add('sur Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+)); */
+
+/* storiesOf('Your text', module).add('Your text', () => (
+  <YourComponent showApp={action('componentReact')} />
+)) */
