401b7b1d3d30f512281f236c9af9d8be87b2baba
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-22 16:26:27 +0200

    files.insertFinalNewline

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,8 @@
 // TODO ffsdfdsf
 
-import React from 'react';
-import { ListGroupItem } from 'reactstrap'
+import React from "react"
+import { ListGroupItem } from "reactstrap"
 
 const Article = () => <ListGroupItem>Article</ListGroupItem>
 
-
 export default Article
-
