f9e284fa63741d953965e861aa713b273b9a75b8
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-23 16:53:00 +0200

    destructuring props in component DashboardCard

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -8,17 +8,19 @@ import {
   Button
 } from "reactstrap"
 
-const DashboardCard = ({ contenu }) => (
-  <div>
-    <Card>
-      <CardImg top width="100%" src={contenu.image} alt="Card image cap" />
-      <CardBody>
-        <CardTitle>{contenu.titre}</CardTitle>
-        <CardText>{contenu.bio}</CardText>
-        <Button src={contenu.link}>{contenu.buttonText}</Button>
-      </CardBody>
-    </Card>
-  </div>
-)
-
+const DashboardCard = ({ contenu }) => {
+  const { image, titre, bio, buttonText, link } = { ...contenu }
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src={image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{titre}</CardTitle>
+          <CardText>{bio}</CardText>
+          <Button src={link}>{buttonText}</Button>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
 export default DashboardCard
