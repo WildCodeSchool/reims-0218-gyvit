cc1e8aadba1f68c6c0f430a84091f89c95bee68b
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-23 13:47:10 +0200

    datas set in state

--- src/App.js
+++ src/App.js
@@ -4,38 +4,43 @@ import "./App.css"
 
 import Content from "./components/Content"
 
-const name = "Kevin"
-
-const toto = [
-  {
-    image: "",
-    titre: "Documents requests",
-    bio:
-      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-    buttonText: "Create first request",
-    link: "lien0"
-  },
-  {
-    image: "",
-    titre: "Share documents",
-    bio:
-      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-    buttonText: "Share documents",
-    link: "lien1"
-  },
-  {
-    image: "",
-    titre: "Store files",
-    bio:
-      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-    buttonText: "Upload documents",
-    link: "lien2"
-  }
-]
-
 class App extends Component {
+  // overwrite of constructor method
+  constructor() {
+    super()
+    this.state = {
+      actionHome: [
+        {
+          image: "",
+          titre: "Documents requests",
+          bio:
+            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+          buttonText: "Create first request",
+          link: "lien0"
+        },
+        {
+          image: "",
+          titre: "Share documents",
+          bio:
+            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+          buttonText: "Share documents",
+          link: "lien1"
+        },
+        {
+          image: "",
+          titre: "Store files",
+          bio:
+            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+          buttonText: "Upload documents",
+          link: "lien2"
+        }
+      ],
+      name: "Kevin"
+    }
+  }
+  // overwrite of render method
   render() {
-    return <Content elements={toto} name={name} />
+    return <Content elements={this.state.actionHome} name={this.state.name} />
   }
 }
 
