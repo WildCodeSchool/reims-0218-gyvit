a871146a78dd715f22de31e13845ef96af175a81
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-23 14:24:54 +0200

    storybook component navbartop

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,21 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import NavbarTop from "../components/NavbarTop"
 
 import { storiesOf } from "@storybook/react"
 
+import NavbarTop from "../components/NavbarTop"
+import NavbarTopSearch from "../components/NavbarTopSearch"
+import NavbarTopNotif from "../components/NavbarTopNotif"
+import NavbarTopProfile from "../components/NavbarTopProfile"
+import NavbarTopNotifBubble from "../components/NavbarTopNotifBubble"
+
 storiesOf("Page Dashboard", module).add("NavbarTop", () => <NavbarTop />)
+
+storiesOf("NavbarTop", module)
+  .add("NavbarTopSearch", () => <NavbarTopSearch />)
+  .add("NavbarTopNotif", () => <NavbarTopNotif />)
+  .add("NavbarTopProfile", () => <NavbarTopProfile />)
+
+storiesOf("NavbarTopNotif", module).add("NavbarTopNotifBubble", () => (
+  <NavbarTopNotifBubble />
+))
