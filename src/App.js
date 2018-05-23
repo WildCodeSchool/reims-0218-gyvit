5452f838ce87071f1131eefd78332f4fe2f5a1ee
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-23 14:40:43 +0200

    clean App, add Content to storybook

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import React, { Component } from "react"
+import { Component } from "react"
 
 import "./App.css"
 
-import Content from "./components/Content"
-
-class App extends Component {
-  // overwrite of constructor method
-  constructor() {
-    super()
-    this.state = {
-      actionHome: [
-        {
-          image: "",
-          titre: "Documents requests",
-          bio:
-            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-          buttonText: "Create first request",
-          link: "lien0"
-        },
-        {
-          image: "",
-          titre: "Share documents",
-          bio:
-            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-          buttonText: "Share documents",
-          link: "lien1"
-        },
-        {
-          image: "",
-          titre: "Store files",
-          bio:
-            "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
-          buttonText: "Upload documents",
-          link: "lien2"
-        }
-      ],
-      name: "Kevin"
-    }
-  }
-  // overwrite of render method
-  render() {
-    return <Content elements={this.state.actionHome} name={this.state.name} />
-  }
-}
+class App extends Component {}
 
 export default App
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React from "react"
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import { linkTo } from "@storybook/addon-links"
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from "@storybook/react/demo"
+import Content from "../components/Content"
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+))
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
+  ))
+
+const actionHome = [
+  {
+    image: "",
+    titre: "Documents requests",
+    bio:
+      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+    buttonText: "Create first request",
+    link: "lien0"
+  },
+  {
+    image: "",
+    titre: "Share documents",
+    bio:
+      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+    buttonText: "Share documents",
+    link: "lien1"
+  },
+  {
+    image: "",
+    titre: "Store files",
+    bio:
+      "Take the first step to launching your store. Add physical items, digital downloads you can dream up",
+    buttonText: "Upload documents",
+    link: "lien2"
+  }
+]
+
+const name = "Kevin"
+
+storiesOf("Content", module).add("with array of actions", () => (
+  <Content elements={actionHome} name={name} />
+))
