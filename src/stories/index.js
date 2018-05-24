cd36b46097a8b91dc4d68e792bf67555c56d7c6c
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-24 10:09:24 +0200

    add update stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,7 +10,6 @@ import NavbarTopNotif from "../components/NavbarTopNotif"
 import NavbarTopProfile from "../components/NavbarTopProfile"
 import NavbarTopNotifBubble from "../components/NavbarTopNotifBubble"
 
-<<<<<<< HEAD
 const Bubble0 = {
   numberNotifs: 0
 }
@@ -26,30 +25,6 @@ const Bubble16 = {
 const Bubble116 = {
   numberNotifs: 116
 }
-=======
-storiesOf("Page home", module).add("navbar", () => (
-  <div>
-    <Nav vertical>
-      <NavItem>
-        <NavLink href="#">Link</NavLink>
-      </NavItem>
-    </Nav>
-    <hr />
-  </div>
-))
-
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
->>>>>>> 92f365aab31af8206f5766f1a7d381bbf63c2412
 
 storiesOf("Page Dashboard", module).add("NavbarTop", () => <NavbarTop />)
 
@@ -65,7 +40,6 @@ storiesOf("NavbarTopNotif", module).add("NavbarTopNotif0", () => (
 storiesOf("NavbarTopNotif", module).add("NavbarTopNotif116", () => (
   <NavbarTopNotif displayNumberNotifs={Bubble116.numberNotifs} />
 ))
-<<<<<<< HEAD
 
 storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 0 notif",
@@ -86,6 +60,4 @@ storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 116 notifs",
   () => <NavbarTopNotifBubble numberNotifs={Bubble116.numberNotifs} />
 )
-=======
 storiesOf("Page Dashboard").add("Listlink", () => <Listlink />)
->>>>>>> 92f365aab31af8206f5766f1a7d381bbf63c2412
