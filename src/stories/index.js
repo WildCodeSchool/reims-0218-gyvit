83e705ec80bf50958635bbd7bf3c60c18a5c52f5
Unknown <tarteserviceinformatique@sfr.fr>
2018-05-24 13:44:27 +0200

    adding component ContentFile to display files or directorie

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,35 +3,14 @@ import { storiesOf } from "@storybook/react"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import ContentFile from "../components/ContentFile"
+
 import Listlink from "../components/ListLink"
 import NavbarTop from "../components/NavbarTop"
 import NavbarTopSearch from "../components/NavbarTopSearch"
 import NavbarTopNotif from "../components/NavbarTopNotif"
 import NavbarTopProfile from "../components/NavbarTopProfile"
 import NavbarTopNotifBubble from "../components/NavbarTopNotifBubble"
-
-storiesOf("Page home", module).add("navbar", () => (
-  <div>
-    <Nav vertical>
-      <NavItem>
-        <NavLink href="#">Link</NavLink>
-      </NavItem>
-    </Nav>
-    <hr />
-  </div>
-))
-
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
 
 storiesOf("Page Dashboard", module).add("NavbarTop", () => <NavbarTop />)
 
@@ -44,3 +23,10 @@ storiesOf("NavbarTopNotif", module).add("NavbarTopNotifBubble", () => (
   <NavbarTopNotifBubble />
 ))
 storiesOf("Page Dashboard").add("Listlink", () => <Listlink />)
+
+const files = []
+const dirs = []
+
+storiesOf("Content of Files").add("ContentFile", () => (
+  <ContentFile files={files} dirs={dirs} />
+))
