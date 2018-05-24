3101708636c110113189ea7b319e60c3e4e58fe8
Pierre <dumaypierre@outlook.fr>
2018-05-24 17:52:00 +0200

    del stories init

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,12 +10,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import Content from "../components/Content"
 
-storiesOf("Welcome", module).add("to Storybook", () => (
-  <Welcome showApp={linkTo("Button")} />
-))
-
-import { storiesOf } from "@storybook/react"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Listlink from "../components/ListLink"
@@ -24,29 +18,6 @@ import NavbarTopSearch from "../components/NavbarTopSearch"
 import NavbarTopNotif from "../components/NavbarTopNotif"
 import NavbarTopProfile from "../components/NavbarTopProfile"
 import NavbarTopNotifBubble from "../components/NavbarTopNotifBubble"
-
-storiesOf("Page home", module).add("navbar", () => (
-  <div>
-    <Nav vertical>
-      <NavItem>
-        <NavLink href="#">Link</NavLink>
-      </NavItem>
-    </Nav>
-    <hr />
-  </div>
-))
-
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
 
 const listCardsInHome = [
   {
