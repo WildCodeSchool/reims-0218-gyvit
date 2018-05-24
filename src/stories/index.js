aa8ef7ec4279e1271c00025f142f27e3e70050ed
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-24 17:27:35 +0200

    resole issue add propsWith0notif etc.

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,19 +10,19 @@ import NavbarTopNotif from "../components/NavbarTopNotif"
 import NavbarTopProfile from "../components/NavbarTopProfile"
 import NavbarTopNotifBubble from "../components/NavbarTopNotifBubble"
 
-const bubble0 = {
+const propsWith0notif = {
   notifsCount: 0
 }
 
-const bubble6 = {
+const propsWith6notif = {
   notifsCount: 6
 }
 
-const bubble16 = {
+const propsWith16notif = {
   notifsCount: 16
 }
 
-const bubble116 = {
+const propsWith116notif = {
   notifsCount: 116
 }
 
@@ -30,34 +30,34 @@ storiesOf("Page Dashboard", module).add("NavbarTop", () => <NavbarTop />)
 
 storiesOf("NavbarTop", module)
   .add("NavbarTopSearch", () => <NavbarTopSearch />)
-  .add("NavbarTopNotif", () => <NavbarTopNotif />)
+  .add("NavbarTopNotif", () => <NavbarTopNotif {...propsWith116notif} />)
   .add("NavbarTopProfile", () => <NavbarTopProfile />)
 
 storiesOf("NavbarTopNotif", module).add("NavbarTopNotif0", () => (
-  <NavbarTopNotif notifsCount={bubble0.notifsCount} />
+  <NavbarTopNotif {...propsWith0notif} />
 ))
 
 storiesOf("NavbarTopNotif", module).add("NavbarTopNotif116", () => (
-  <NavbarTopNotif notifsCount={bubble116.notifsCount} />
+  <NavbarTopNotif {...propsWith116notif} />
 ))
 
 storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 0 notif",
-  () => <NavbarTopNotifBubble notifsCount={bubble0.notifsCount} />
+  () => <NavbarTopNotifBubble {...propsWith0notif} />
 )
 
 storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 6 notifs",
-  () => <NavbarTopNotifBubble notifsCount={bubble6.notifsCount} />
+  () => <NavbarTopNotifBubble {...propsWith6notif} />
 )
 
 storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 16 notifs",
-  () => <NavbarTopNotifBubble notifsCount={bubble16.notifsCount} />
+  () => <NavbarTopNotifBubble {...propsWith16notif} />
 )
 
 storiesOf("NavbarTopNotifBubble", module).add(
   "NavbarTopNotifBubble pour 116 notifs",
-  () => <NavbarTopNotifBubble notifsCount={bubble116.notifsCount} />
+  () => <NavbarTopNotifBubble {...propsWith116notif} />
 )
 storiesOf("Page Dashboard").add("Listlink", () => <Listlink />)
