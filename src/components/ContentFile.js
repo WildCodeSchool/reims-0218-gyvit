2a830e90b1618f1dc3185174db6fd4866bcc04c3
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-25 12:04:10 +0200

    complete tbody with Dirs & Files

--- src/components/ContentFile.js
+++ src/components/ContentFile.js
@@ -1,7 +1,7 @@
 import React from "react"
+const moment = require("moment-timezone")
 
 const ContentFile = ({ files, dirs }) => {
-  console.log(dirs)
   return (
     <tbody>
       {dirs.map((dir, index) => {
@@ -11,9 +11,25 @@ const ContentFile = ({ files, dirs }) => {
               <i src="" alt="Directory Icon" />
             </th>
             <th>{dir.name}</th>
-            <th>{dir.modified}</th>
+            <th>{dir.modified + "   " + Date.parse(dir.modified)}</th>
             <th>
               {dir.shares.map((share, index) => (
+                <i src="" alt={`icone Share n° ${index}`} />
+              ))}
+            </th>
+          </tr>
+        )
+      })}
+      {files.map((file, index) => {
+        return (
+          <tr>
+            <th>
+              <i src="" alt="file Icon" />
+            </th>
+            <th>{file.name}</th>
+            <th>{file.modified + "   " + Date.parse(file.modified)}</th>
+            <th>
+              {file.shares.map((share, index) => (
                 <i src="" alt={`icone Share n° ${index}`} />
               ))}
             </th>
