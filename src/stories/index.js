8e22596252aa7e273f65e359c7845e170ac09b06
Khalid <khalid51000@gmail.com>
2018-05-25 11:21:04 +0200

    add props to listlinks and css

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,31 +1,58 @@
 import React from "react"
-
 import { storiesOf } from "@storybook/react"
-import { action } from "@storybook/addon-actions"
 import { linkTo } from "@storybook/addon-links"
-import { NavItem, Nav, NavLink } from "reactstrap"
-import { Button, Welcome } from "@storybook/react/demo"
+import { Welcome } from "@storybook/react/demo"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-storiesOf("Page home", module).add("navbar", () => (
-  <div>
-    <Nav vertical>
-      <NavItem>
-        <NavLink href="#">Link</NavLink>
-      </NavItem>
-    </Nav>
-    <hr />
-  </div>
+import Link from "../components/Link"
+import Listlink from "../components/ListLink"
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
 ))
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
+const listLinksProps = {
+  dataLinks: [
+    {
+      nameLink: "Home",
+      icon: process.env.PUBLIC_URL + "/img/house_38533.ico"
+    },
+    {
+      nameLink: "Files",
+      icon: process.env.PUBLIC_URL + "/img/files.ico"
+    },
+    {
+      nameLink: "Shares",
+      icon: process.env.PUBLIC_URL + "/img/share.ico"
+    },
+    {
+      nameLink: "Templates",
+      icon: process.env.PUBLIC_URL + "/img/templates.ico"
+    },
+    {
+      nameLink: "Requests",
+      icon: process.env.PUBLIC_URL + "/img/request.ico"
+    },
+    {
+      nameLink: "Public links",
+      icon: process.env.PUBLIC_URL + "/img/links.ico"
+    },
+    {
+      nameLink: "Contacts",
+      icon: process.env.PUBLIC_URL + "/img/contact.ico"
+    },
+    {
+      nameLink: "Settings",
+      icon: process.env.PUBLIC_URL + "/img/settings.ico"
+    }
+  ]
+}
+
+const homeLinkProps = {
+  nameLink: "Home",
+  icon: process.env.PUBLIC_URL + "/img/house_38533.ico"
+}
+
+storiesOf("Page Dashboard")
+  .add("Listlink", () => <Listlink {...listLinksProps} />)
+  .add("Link", () => <Link {...homeLinkProps} />)
