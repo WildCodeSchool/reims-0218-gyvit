990ffa8143bfc5d9db683d2d87bb89c4e41bcf16
Unknown <tarteserviceinformatique@sfr.fr>
2018-05-29 15:10:18 +0200

    formatting date

--- src/components/ContentFile.js
+++ src/components/ContentFile.js
@@ -1,17 +1,21 @@
 import React from "react"
-const moment = require("moment-timezone")
+import { DateTime } from "luxon"
 
 const ContentFile = ({ files, dirs }) => {
   return (
     <tbody>
       {dirs.map((dir, index) => {
+        const dateMilli = Date.parse(dir.modified)
+        const dateToDisplay = DateTime.fromMillis(dateMilli).toLocaleString(
+          DateTime.DATETIME_SHORT
+        )
         return (
           <tr>
             <th>
               <i src="" alt="Directory Icon" />
             </th>
             <th>{dir.name}</th>
-            <th>{dir.modified + "   " + Date.parse(dir.modified)}</th>
+            <th>{dir.modified + "   " + dateToDisplay}</th>
             <th>
               {dir.shares.map((share, index) => (
                 <i src="" alt={`icone Share n° ${index}`} />
@@ -21,13 +25,14 @@ const ContentFile = ({ files, dirs }) => {
         )
       })}
       {files.map((file, index) => {
+        const dateMilli = Date.parse(file.modified)
         return (
           <tr>
             <th>
               <i src="" alt="file Icon" />
             </th>
             <th>{file.name}</th>
-            <th>{file.modified + "   " + Date.parse(file.modified)}</th>
+            <th>{file.modified + "   " + dateMilli.toLocaleString("fr-fr")}</th>
             <th>
               {file.shares.map((share, index) => (
                 <i src="" alt={`icone Share n° ${index}`} />
