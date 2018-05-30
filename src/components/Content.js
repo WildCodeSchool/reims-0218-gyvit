1a846cebe77d026d997ad5a64b5554cfb23f347a
Thib <thibaud.royer051@gmail.com>
2018-05-30 11:29:45 +0200

    boobstrap in content

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,18 +7,17 @@ import DashboardCard from "./DashboardCard"
 const Content = ({ elements, name }) => (
   <Container>
     <Row className="App">
-      <Col sm="12">
+      <Col sm="12" md={{ size: 4, offset: 4 }}>
         <h1>Welcome to Gyvit, {name}.</h1>
       </Col>
-      <Col sm="12">
+      <Col sm="12" md={{ size: 4, offset: 4 }}>
         <h2>What would you like to do ?</h2>
       </Col>
     </Row>
     <Row>
       {elements.map(element => {
-        console.log(element)
         return (
-          <Col xs="4">
+          <Col xs="6" sm="4">
             <DashboardCard key={element.id} contenu={element} />
           </Col>
         )
