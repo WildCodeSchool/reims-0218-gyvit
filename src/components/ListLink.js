258d2c07bad30a9667762dd26d3df92c93a3cb2f
Anthony <elmorodjeur@gmail.com>
2018-05-30 17:13:59 +0200

    add title

--- src/components/ListLink.js
+++ src/components/ListLink.js
@@ -3,24 +3,36 @@
 import React from "react"
 import Link from "./Link"
 import { Nav, NavItem, Navbar } from "reactstrap"
+import NavbarLeftLogo from "./NavbarLeftLogo"
 
 const ListLink = ({ dataLinks }) => (
-  <Navbar
-    light
-    expand="md"
-    style={{
-      width: "220px",
-      height: "1024px",
-      backgroundColor: "#231b56",
-      color: "white"
-    }}
-  >
-    <Nav vertical>
-      <NavItem>
-        {dataLinks.map((link, index) => <Link key={index} {...link} />)}
-      </NavItem>
-    </Nav>
-  </Navbar>
+  <div>
+    <Navbar
+      light
+      expand="md"
+      style={{
+        width: "220px",
+        height: "1024px",
+        backgroundColor: "#231b56",
+        color: "white",
+        marginTop: 10
+      }}
+    >
+      <Nav vertical>
+        <NavbarLeftLogo
+          style={{
+            color: "red",
+            marginTop: 100,
+            paddingRight: 100
+          }}
+        />
+
+        <NavItem>
+          {dataLinks.map((link, index) => <Link key={index} {...link} />)}
+        </NavItem>
+      </Nav>
+    </Navbar>
+  </div>
 )
 
 export default ListLink
---
+++ src/components/NavbarLeftLogo.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const NavbarLeftLogo = () => {
+  return (
+    <div
+      style={{
+        color: "white",
+        textAlign: "center",
+        fontWeight: "bold"
+      }}
+    >
+      <p
+        style={{
+          color: "white",
+          textAlign: "center",
+          fontWeight: "bold"
+        }}
+      >
+        {" "}
+        GYVIT{" "}
+      </p>
+    </div>
+  )
+}
+
+export default NavbarLeftLogo
