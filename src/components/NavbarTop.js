6e8153a4442963838d0cd357958c89b0bbad78a6
Pierre <dumaypierre@outlook.fr>
2018-05-30 12:07:21 +0200

    css on component NavBarTop

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -7,8 +7,20 @@ import NavbarTopNotif from "./NavbarTopNotif"
 import NavbarTopSearch from "./NavbarTopSearch"
 import NavbarTopProfile from "./NavbarTopProfile"
 
-const NavbarTop = (propsWith116notif, propsContentOfProfil) => (
-  <Nav className="row">
+const propsContentOfProfil = {
+  lastname: "Duflot",
+  firstname: "Romain",
+  business: "Notoriety"
+}
+const NavbarTop = (propsWith116notif, firstname, lastname, business) => (
+  <Nav
+    style={{
+      height: "90px",
+      "background-color": "#ffffff",
+      border: "solid 1px #e5e8f4"
+    }}
+    className="row"
+  >
     <NavbarTopSearch />
     <NavbarTopNotif {...propsWith116notif} />
     <NavbarTopProfile {...propsContentOfProfil} />
