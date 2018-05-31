39d776ff3b9683aad25b8229fa5f830ad0827654
Khalid <khalid51000@gmail.com>
2018-05-31 09:59:29 +0200

    add container fluid

--- src/components/SignInContent.js
+++ src/components/SignInContent.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, Row, Container } from "reactstrap"
+import { Button, Row, Container, Col } from "reactstrap"
 
 const SignInContent = () => (
   <div
@@ -11,45 +11,42 @@ const SignInContent = () => (
       position: "relative"
     }}
   >
-    <Container-fluid>
+    <Container fluid>
       <Row>
-        <p
-          style={{
-            "margin-top": "49px",
-            "margin-left": "280px",
-            "padding-right": "50px",
-            "font-size": "16px",
-            "font-weight": "normal",
-            "font-style": "normal",
-            "font-stretch": "normal",
-            "line-height": "normal",
-            "letter-spacing": "normal",
-            "text-align": "left",
-            color: "#3b425d",
-            position: "absolute"
-          }}
-        >
-          Don't have an account ?
-        </p>
-
-        <Button
-          outline
-          style={{
-            width: "156px",
-            height: "54px",
-            "border-radius": "100px",
-            border: "solid 1px #7a57d1",
-            color: "#7a57d1",
-            "margin-top": "30px",
-            "margin-left": "475px",
-
-            textColor: "color: #7a57d1;"
-          }}
-        >
-          Get Started
-        </Button>
+        <Col xs="9">
+          <p
+            style={{
+              "margin-top": "49px",
+              "font-size": "16px",
+              "font-weight": "normal",
+              "font-style": "normal",
+              "letter-spacing": "normal",
+              "text-align": "left",
+              color: "#3b425d",
+              position: "absolute",
+              "margin-left": "470px"
+            }}
+          >
+            Don't have an account ?
+          </p>
+        </Col>
+        <Col xs="3">
+          <Button
+            outline
+            style={{
+              width: "156px",
+              height: "54px",
+              "border-radius": "100px",
+              border: "solid 1px #7a57d1",
+              color: "#7a57d1",
+              "margin-top": "30px"
+            }}
+          >
+            Get Started
+          </Button>
+        </Col>
       </Row>
-    </Container-fluid>
+    </Container>
   </div>
 )
 
