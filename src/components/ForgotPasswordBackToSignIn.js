95ef2da94228ddf28c4aaa58724d7731ba3e621b
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-31 12:04:11 +0200

    delete {}

--- src/components/ForgotPasswordBackToSignIn.js
+++ src/components/ForgotPasswordBackToSignIn.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {} from "reactstrap"
 
 const ForgotPasswordBackToSignIn = () => <p>Back to Sign in</p>
 
--- src/components/ForgotPasswordGetStarted.js
+++ src/components/ForgotPasswordGetStarted.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {} from "reactstrap"
 
 const ForgotPasswordGetStarted = () => (
   <div style={{ "padding-top": "20%" }}>
