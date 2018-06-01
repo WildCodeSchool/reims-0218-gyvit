208a16158481425fd204bd66eb08a6367d72f1e0
Pierre <dumaypierre@outlook.fr>
2018-06-01 16:46:02 +0200

    no commit

--- src/components/PageFolders/PageFolders.js
+++ src/components/PageFolders/PageFolders.js
@@ -1,11 +1,25 @@
 import React from "react"
-
+import { Container, Row, Col } from "reactstrap"
 import FoldersBarTop from "./FoldersBarTop"
+import FoldersTable from "./FoldersTable"
+import NavbarTop from "../NavbarTop/NavbarTop"
+import NavbarLeft from "../NavbarLeft/NavbarLeft"
+import NavbarLeftItem from "../NavbarLeft/NavbarLeftItem"
+import FoldersTableCategory from "./FoldersTableCategory"
 
-const PageFolders = ({ files = [], dirs = [] }) => (
-  <div>
-    <FoldersBarTop />
-  </div>
+const PageFolders = ({ dataLinks, files = [], dirs = [] }) => (
+  <Container fluid>
+    <Row>
+      <Col xs="2">
+        <NavbarLeft dataLinks={dataLinks} />
+      </Col>
+      <Col xs="10">
+        <NavbarTop />
+        <FoldersBarTop />
+        <FoldersTable />
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default PageFolders
