ec7ef02c7509095d5dfd49c8a0e826e2837a0586
Pierre <dumaypierre@outlook.fr>
2018-06-06 17:02:31 +0200

    merde for add menuleft

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import reducers from "./reducers"
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
 import App from "./App"
@@ -61,4 +65,15 @@ fetch(urlFiles, {
 //   })
 
 ReactDOM.render(<App />, document.getElementById("root"))
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
 registerServiceWorker()
