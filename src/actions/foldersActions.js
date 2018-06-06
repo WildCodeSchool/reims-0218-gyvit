cbe8ea9a47aa10915db825b7118720cb027d715f
Khalid <khalid51000@gmail.com>
2018-06-06 17:28:14 +0200

    addFoldersReducer

--- src/actions/foldersActions.js
+++ src/actions/foldersActions.js
@@ -1,6 +1,12 @@
 export const FETCH_FOLDERS_SUCCESS = "FETCH_FOLDERS_SUCCESS"
+export const ADD_FOLDERS_ACTIONS = "ADD_FOLDERS_ACTIONS"
 
 export const makeFetchFoldersSuccess = response => ({
   type: FETCH_FOLDERS_SUCCESS,
+  response
+})
+
+export const makeAddFoldersSuccess = response => ({
+  type: ADD_FOLDERS_ACTIONS,
   response
 })
--- src/reducers/filesReducer.js
+++ src/reducers/filesReducer.js
@@ -1,12 +1,17 @@
-import { FETCH_FILES_SUCCESS } from "../actions/filesActions"
+import { FETCH_FILES_SUCCESS, ADD_FILE_ACTION } from "../actions/filesActions"
 
 const initialState = []
 
-const files = (prevState = initialState, action) => {
+export const files = (prevState = initialState, action) => {
   if (action.type === FETCH_FILES_SUCCESS) {
     return action.response
   }
   return prevState
 }
 
-export default files
+export const addFiles = (prevState = initialState, action) => {
+  if (action.type === ADD_FILE_ACTION) {
+    return [...prevState, action.response]
+  }
+  return prevState
+}
