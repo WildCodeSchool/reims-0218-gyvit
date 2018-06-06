b481be46878c96557f24791abc6fc15bfd20a08a
Pierre <dumaypierre@outlook.fr>
2018-06-06 10:35:13 +0200

    add fetch directorys and files

---
+++ src/api/directorys.js
@@ -0,0 +1,18 @@
+const urlDirectorys =
+  "https://dev.gyvit.io/api/storage/directorys/list?name=amm"
+
+const headersDirectorys = new Headers({
+  curl: "request GET",
+  url: "https://dev.gyvit.io/api/storage/directorys/list?name=amm",
+  header: "Accept: application/json",
+  header:
+    "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImV4cCI6MTUyODcyNTAyMH0.dgVTGliH4ag6IChVgwsqjctmsMlwalY_3sqoSPQu0c8",
+  header: "Content-Type: application/json"
+})
+fetch(urlDirectorys, headersDirectorys)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
