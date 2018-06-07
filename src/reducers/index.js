32781fc70d121a33c626b8a4e800c1c125124ff3
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-07 12:00:00 +0200

    delete reducer addFileReducer should not exist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from "redux"
 import userReducer from "./userReducer"
 import foldersReducers from "./foldersReducer"
 import filesReducers from "./filesReducer"
-import addFilesReducers from "./addFilesReducer"
 
 export default combineReducers({
   folders: foldersReducers,
   files: filesReducers,
-  addFiles: addFilesReducers,
   user: userReducer
 })
