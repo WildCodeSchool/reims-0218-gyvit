19f076ee1360646b91a6d4f53d7f55f950e719c3
Anthony <elmorodjeur@gmail.com>
2018-06-11 09:55:12 +0200

    to change branch

--- src/actions/filesActions.test.js
+++ src/actions/filesActions.test.js
@@ -5,6 +5,10 @@ import {
   makeAddAFileSuccess
 } from "./filesActions"
 
+
+import {DELETE_A_FOLDER_SUCCESS,
+
+  deleteFolderAction} from "./foldersActions"
 //action FETCH_FILES_SUCCESS
 
 const prevState = [
@@ -191,3 +195,159 @@ describe("action add a file", () => {
     ).toEqual(expectedAddAFileAction)
   })
 })
+      //Action delete folder
+
+      describe("action make User in State", () => {
+        it("should return DELETE_FOLDER_ACTION", () => {
+          const prevState = [
+                  {
+                    _id: "dir_DOl2kN3n9lMeedN90kL9",
+                    object: "directory",
+                    name: "SammTrading",
+                    created: "2018-03-29T00:00:00+00:00",
+                    modified: "2018-03-29T00:00:00+00:00",
+                    shares: [],
+                    files: [],
+                    dirs: [
+                      {
+                        _id: "dir_EoeInRgUgzMPh0aLN2nz",
+                        object: "directory",
+                        name: "Webshop",
+                        created: "2018-03-29T11:36:42+00:00",
+                        modified: "2018-03-29T11:36:42+00:00",
+                        shares: []
+                      }
+                    ],
+                    path: [
+                      {
+                        _id: "dir_kL9D9lMe3ndN90eOl2kN",
+                        object: "directory",
+                        name: "Meduza",
+                        created: "2018-03-29T00:00:00+00:00",
+                        modified: "2018-03-29T00:00:00+00:00",
+                        index: 0
+                      },
+                      {
+                        _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                        object: "directory",
+                        name: "Clients",
+                        created: "2018-03-29T00:00:00+00:00",
+                        modified: "2018-03-29T00:00:00+00:00",
+                        index: 1
+                      }
+                    ],
+                    parent: {
+                      _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                      object: "directory",
+                      name: "Clients",
+                      created: "2018-03-29T00:00:00+00:00",
+                      modified: "2018-03-29T00:00:00+00:00",
+                      index: 1
+                    }
+                  }
+                
+                ]
+                [
+                  {
+                    _id: "dir_DOl2kN3n9lMeedN90kL9",
+                    object: "directory",
+                    name: "SammTrading",
+                    created: "2018-03-29T00:00:00+00:00",
+                    modified: "2018-03-29T00:00:00+00:00",
+                    shares: [],
+                    files: [],
+                    dirs: [
+                      {
+                        _id: "dir_EoeInRgUgzMPh0aLN2nz",
+                        object: "directory",
+                        name: "Webshop",
+                        created: "2018-03-29T11:36:42+00:00",
+                        modified: "2018-03-29T11:36:42+00:00",
+                        shares: []
+                      }
+                    ],
+                    path: [
+                      {
+                        _id: "dir_kL9D9lMe3ndN90eOl2kN",
+                        object: "directory",
+                        name: "Meduza",
+                        created: "2018-03-29T00:00:00+00:00",
+                        modified: "2018-03-29T00:00:00+00:00",
+                        index: 0
+                      },
+                      {
+                        _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                        object: "directory",
+                        name: "Clients",
+                        created: "2018-03-29T00:00:00+00:00",
+                        modified: "2018-03-29T00:00:00+00:00",
+                        index: 1
+                      }
+                    ],
+                    parent: {
+                      _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                      object: "directory",
+                      name: "Clients",
+                      created: "2018-03-29T00:00:00+00:00",
+                      modified: "2018-03-29T00:00:00+00:00",
+                      index: 1
+                    }
+                  }
+                ]
+            
+          const expected = {
+            type: DELETE_FOLDER_ACTION,
+             response:[
+            {
+              _id: "dir_DOl2kN3n9lMeedN90kL9",
+              object: "directory",
+              name: "SammTrading",
+              created: "2018-03-29T00:00:00+00:00",
+              modified: "2018-03-29T00:00:00+00:00",
+              shares: [],
+              files: [],
+              dirs: [
+                {
+                  _id: "dir_EoeInRgUgzMPh0aLN2nz",
+                  object: "directory",
+                  name: "Webshop",
+                  created: "2018-03-29T11:36:42+00:00",
+                  modified: "2018-03-29T11:36:42+00:00",
+                  shares: []
+                }
+              ],
+              path: [
+                {
+                  _id: "dir_kL9D9lMe3ndN90eOl2kN",
+                  object: "directory",
+                  name: "Meduza",
+                  created: "2018-03-29T00:00:00+00:00",
+                  modified: "2018-03-29T00:00:00+00:00",
+                  index: 0
+                },
+                {
+                  _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                  object: "directory",
+                  name: "Clients",
+                  created: "2018-03-29T00:00:00+00:00",
+                  modified: "2018-03-29T00:00:00+00:00",
+                  index: 1
+                }
+              ],
+              parent: {
+                _id: "dir_3ndN90kL9D9lMeeOl2kN",
+                object: "directory",
+                name: "Clients",
+                created: "2018-03-29T00:00:00+00:00",
+                modified: "2018-03-29T00:00:00+00:00",
+                index: 1
+              }
+            }
+          
+          ]} 
+          expect(deleteFolderAction(prevState)).toEqual(expected)
+        })
+      })
+      
+      
+      
--- src/actions/foldersActions.js
+++ src/actions/foldersActions.js
@@ -1,6 +1,7 @@
 export const FETCH_FOLDERS_SUCCESS = "FETCH_FOLDERS_SUCCESS"
 export const ADD_A_FOLDER_ACTION = "ADD_A_FOLDER_ACTION"
-export const DELETE_FOLDER_SUCCES = "DELETE_FOLDER_SUCCES "
+export const DELETE_FOLDER_ACTION = "DELETE_FOLDER_SUCCES "
+export const DELETE_A_FOLDER_SUCCESS = "DELETE_FOLDER_ACTION "
 
 
 export const makeFetchFoldersSuccess = response => ({
@@ -14,6 +15,11 @@ export const makeAddAFolderSuccess = response => ({
 })
 
 export const makeDeleteFolderSucces = folderId => ({
-  type: DELETE_FOLDER_SUCCES,
+  type: DELETE_A_FOLDER_SUCCESS,
+  folderId
+ })
+
+ export const deleteFolderAction = folderId => ({
+  type: DELETE_FOLDER_ACTION,
   folderId
  })
