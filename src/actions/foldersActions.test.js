e24708b744153e19768fc30dffe475a53d377ca9
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-11 10:33:37 +0200

    action UPDATE_A_FOLDER_SUCCESS and update in folder reducer , tests are ok

--- src/actions/foldersActions.test.js
+++ src/actions/foldersActions.test.js
@@ -2,7 +2,9 @@ import {
   FETCH_FOLDERS_SUCCESS,
   makeFetchFoldersSuccess,
   ADD_A_FOLDER_SUCCESS,
-  makeAddAFolderSuccess
+  makeAddAFolderSuccess,
+  UPDATE_A_FOLDER_SUCCESS,
+  makeUpdateAFolderSuccess
 } from "./foldersActions"
 
 //action FETCH_FOLDERS_SUCCESS
@@ -211,4 +213,58 @@ describe("action add a folder", () => {
       })
     ).toEqual(expectedAddAFolderAction)
   })
+})
+
+// action UPDATE_A_FOLDER_ACTION
+it("should return an action UPDATE_A_FOLDER_SUCCESS", () => {
+  const response = {
+    _id: "dir_DOl2kN3n9lMeedN90kL9",
+    object: "directory",
+    name: "New name",
+    created: "2018-03-29T00:00:00+00:00",
+    modified: "2018-03-29T00:00:00+00:00",
+    shares: [],
+    files: [],
+    dirs: [
+      {
+        _id: "dir_EoeInRgUgzMPh0aLN2nz",
+        object: "directory",
+        name: "Webshop",
+        created: "2018-03-29T11:36:42+00:00",
+        modified: "2018-03-29T11:36:42+00:00",
+        shares: []
+      }
+    ],
+    path: [
+      {
+        _id: "dir_kL9D9lMe3ndN90eOl2kN",
+        object: "directory",
+        name: "Meduza",
+        created: "2018-03-29T00:00:00+00:00",
+        modified: "2018-03-29T00:00:00+00:00",
+        index: 0
+      },
+      {
+        _id: "dir_3ndN90kL9D9lMeeOl2kN",
+        object: "directory",
+        name: "Clients",
+        created: "2018-03-29T00:00:00+00:00",
+        modified: "2018-03-29T00:00:00+00:00",
+        index: 1
+      }
+    ],
+    parent: {
+      _id: "dir_3ndN90kL9D9lMeeOl2kN",
+      object: "directory",
+      name: "Clients",
+      created: "2018-03-29T00:00:00+00:00",
+      modified: "2018-03-29T00:00:00+00:00",
+      index: 1
+    }
+  }
+  const expected = {
+    type: UPDATE_A_FOLDER_SUCCESS,
+    response
+  }
+  expect(makeUpdateAFolderSuccess(response)).toEqual(expected)
 })
