f14eb038f66f00f0418fe8953c270e4d0e0d36a5
Thib <thibaud.royer051@gmail.com>
2018-06-19 17:26:28 +0200

    +

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react"
 import "./App.css"
 // import logo from "./logo.svg"
 // import NavbarTopFilter from "./containers/SearchContainer"
-import PageFolders from "./components/PageFolders/PageFolders"
+import PageDashboard from "./components/PageDashboard/PageDashboard"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PageFolders />
+        <PageDashboard />
       </div>
     )
   }
