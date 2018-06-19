8a23370b8f31ed74689818be759207bc337e7e9b
Anthony <elmorodjeur@gmail.com>
2018-06-19 11:33:37 +0200

    enhance container

--- src/containers/DashboardListCardsContainer.js
+++ src/containers/DashboardListCardsContainer.js
@@ -1,8 +1,6 @@
-import React, {Components}from "react"
+import React, {Component}from "react"
 import { Container, Row, Col } from "reactstrap"
 import { connect } from "react-redux"
-
-import DashboardCard from "./DashboardCard"
 
 const mapStateToProps = state => ({
   elements: state.elements,
@@ -10,57 +8,49 @@ const mapStateToProps = state => ({
 })
 
 class DashboardListCardsContainer extends Component {
-render(){<Container>
-  <Row className="App">
-    <Col md="12" sm="4">
-      <p
-        style={{
-          fontSize: "24px",
-          height: "33px",
-          marginTop: "120px",
-          textAlign: "center"
-        }}
-      >
-        Welcome to Gyvit, {name}.
-      </p>
-    </Col>
-    <Col sm="12" md={{ size: 4, offset: 3 }}>
-      <p
-        style={{
-          fontSize: "18px",
-          height: "24px",
-          textAlign: "center",
-          color: "#000000",
-          opacity: 0.45,
-          marginBottom: "60px"
-        }}
-      >
-        What would you like to do ?
-      </p>
-    </Col>
-  </Row>
-  <Row>
-    {elements.map(element => {      
-       return <DashboardCard key={element.id} {...element} />
-    })}
-  </Row>
-</Container>
+render(){
   return (
-
+  <Container>
+    <Row className="App">
+      <Col md="12" sm="4">
+        <p
+          style={{
+            fontSize: "24px",
+            height: "33px",
+            marginTop: "120px",
+            textAlign: "center"
+          }}
+        >
+          Welcome to Gyvit, {this.state.name}.
+        </p>
+      </Col>
+      <Col sm="12" md={{ size: 4, offset: 3 }}>
+        <p
+          style={{
+            fontSize: "18px",
+            height: "24px",
+            textAlign: "center",
+            color: "#000000",
+            opacity: 0.45,
+            marginBottom: "60px"
+          }}
+        >
+          What would you like to do ?
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      {/* {elements.map(element => (
+         <DashboardCard key={element.id} {...element} />
+      ))} */}
+      elements={this.state.elements}
+    </Row>
+  </Container>
   )
 }
-
-
-
-
-
-const DashboardListCards = ({ elements, name }) => (
-  
-)
-
-
+}
 export default connect(
   mapStateToProps
-)(DashboardListCards)
+)(DashboardListCardsContainer)
 
 
