d683097f0cdb917a5888e2f7fb7fcec570684740
Thib <thibaud.royer051@gmail.com>
2018-06-19 09:39:59 +0200

    create folder links

---
+++ src/links/index.js
@@ -0,0 +1,35 @@
+const link = [
+  {
+    nameLink: "Home",
+    icon: process.env.PUBLIC_URL + "/img/house_38533.ico"
+  },
+  {
+    nameLink: "Files",
+    icon: process.env.PUBLIC_URL + "/img/files.ico"
+  },
+  {
+    nameLink: "Shares",
+    icon: process.env.PUBLIC_URL + "/img/share.ico"
+  },
+  {
+    nameLink: "Templates",
+    icon: process.env.PUBLIC_URL + "/img/templates.ico"
+  },
+  {
+    nameLink: "Requests",
+    icon: process.env.PUBLIC_URL + "/img/request.ico"
+  },
+  {
+    nameLink: "Public links",
+    icon: process.env.PUBLIC_URL + "/img/links.ico"
+  },
+  {
+    nameLink: "Contacts",
+    icon: process.env.PUBLIC_URL + "/img/contact.ico"
+  },
+  {
+    nameLink: "Settings",
+    icon: process.env.PUBLIC_URL + "/img/settings.ico"
+  }
+]
+export default link
