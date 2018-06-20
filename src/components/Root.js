f551c6e398d0427fc55c671ef3e0883fe73abcf6
Pierre <dumaypierre@outlook.fr>
2018-06-20 17:57:12 +0200

    first step

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Provider } from "react-redux"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom"
+
 import PageSignIn from "../components/PageSignIn/PageSignIn"
 import PageFolders from "../components/PageFolders/PageFolders"
 import PageGetStarted from "../components/PageGetStarted/PageGetStarted"
 import PageForgetPassword from "../components/PageForgotPassword/PageForgotPassword"
 import PageDashboard from "../components/PageDashboard/PageDashboard"
 import App from "../App"
+import { retrieveMe } from "../api/users/retrieveMe"
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
@@ -24,6 +32,39 @@ const Root = ({ store }) => (
     </Router>
   </Provider>
 )
+/* 
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      retrieveMe().isAuthenticated ? (
+        <PageDashboard />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/sign-in"
+          }}
+        />
+      )
+    }
+  />
+)
+login = () => {
+  fakeAuth.authenticate(() => {
+    this.setState({ redirectToReferrer: true })
+  })
+} */
 
 Root.propTypes = {
   store: PropTypes.object.isRequired
