0cb31aa8cc0fde11c53afea34f4f64b5ae49a5b3
Pierre <dumaypierre@outlook.fr>
2018-06-22 01:34:53 +0200

    add the real icon

--- src/links/index.js
+++ src/links/index.js
@@ -1,42 +1,42 @@
 const link = [
   {
     nameLink: "Home",
-    icon: process.env.PUBLIC_URL + "/img/house_38533.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_home.svg",
     link: "/dashboard"
   },
   {
     nameLink: "Files",
-    icon: process.env.PUBLIC_URL + "/img/files.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_files.svg",
     link: "/dirs"
   },
   {
     nameLink: "Shares",
-    icon: process.env.PUBLIC_URL + "/img/share.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_shares.svg",
     link: "/dirs"
   },
   {
     nameLink: "Templates",
-    icon: process.env.PUBLIC_URL + "/img/templates.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_templates.svg",
     link: "#"
   },
   {
     nameLink: "Requests",
-    icon: process.env.PUBLIC_URL + "/img/request.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_requests.svg",
     link: "#"
   },
   {
     nameLink: "Public links",
-    icon: process.env.PUBLIC_URL + "/img/links.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_links.svg",
     link: "#"
   },
   {
     nameLink: "Contacts",
-    icon: process.env.PUBLIC_URL + "/img/contact.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_contact.svg",
     link: "#"
   },
   {
     nameLink: "Settings",
-    icon: process.env.PUBLIC_URL + "/img/settings.ico",
+    icon: process.env.PUBLIC_URL + "/img/icon_settings.svg",
     link: "#"
   }
 ]
