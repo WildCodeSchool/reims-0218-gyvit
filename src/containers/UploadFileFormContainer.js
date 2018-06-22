edea4cfd48dccee398c7efa60bf55229ee289e9e
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-22 03:11:29 +0200

    delete file in state

--- src/containers/UploadFileFormContainer.js
+++ src/containers/UploadFileFormContainer.js
@@ -14,13 +14,10 @@ const mapDispatchToProps = dispatch => ({
 })
 
 class UploadFileFormContainer extends Component {
-  //add constructor for two input "name" & "file"
   constructor(props) {
     super(props)
     this.state = {
-      //value default of "name" & "file"
-      name: "",
-      file: {}
+      name: ""
     }
     this.onHandleName = this.onHandleName.bind(this)
     this.onHandleFileUpload = this.onHandleFileUpload.bind(this)
