8be3a14df38c552ed0516cf759f3895cec809e4c
Anthony <elmorodjeur@gmail.com>
2018-06-25 15:51:36 +0200

    to share

---
+++ src/reducers/NotifReducer.js
@@ -0,0 +1,19 @@
+
+const initialState = [{
+  id: "1",
+  message: "Coucou"
+},
+{
+  id: "2",
+  message:"Salut"
+}]
+
+//console.log(initialState)
+
+const NotifReducer = (prevState= initialState, action)=> {
+ 
+  return prevState
+}
+
+
+export default NotifReducer
