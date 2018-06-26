dd3f23060cab0997063b92cc6780f641fd6326a1
Khalid <khalid51000@gmail.com>
2018-06-26 10:13:32 +0200

    add react-select

--- src/components/NavbarTop/NavbarTopSearch.js
+++ src/components/NavbarTop/NavbarTopSearch.js
@@ -5,28 +5,28 @@
 
 import React from "react"
 import { FormGroup, Input } from "reactstrap"
+import Async from "react-select"
+import "react-select/dist/react-select.css"
 
 const NavbarTopSearch = ({ search, onSearchType }) => (
   <div>
-    <FormGroup>
-      <span className="fa fa-search" />
-      <Input
-        style={{
-          marginTop: "22px",
-          marginLeft: "6px",
-          height: "46px",
-          backgroundColor: "#fbfcfd",
-          borderRadius: "2px",
-          border: "none"
-        }}
-        type="search"
-        name="search"
-        id="exampleSearch"
-        placeholder="Search..."
-        onChange={onSearchType}
-        value={search}
-      />
-    </FormGroup>
+    <Async
+      style={{
+        marginTop: "22px",
+        marginLeft: "6px",
+        height: "46px",
+        backgroundColor: "#fbfcfd",
+        borderRadius: "2px",
+        border: "none"
+      }}
+      type="search"
+      name="form-field-name"
+      id="exampleSearch"
+      placeholder="Search..."
+      onChange={onSearchType}
+      value={search}
+      options={this.onSearchType}
+    />
   </div>
 )
 
