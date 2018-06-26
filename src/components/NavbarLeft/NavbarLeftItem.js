7cdb8d6cb8805817b80fb5158a6c1cf3a696494f
Pierre <dumaypierre@outlook.fr>
2018-06-26 10:35:37 +0200

    fix all error on dashboard

--- src/components/NavbarLeft/NavbarLeftItem.js
+++ src/components/NavbarLeft/NavbarLeftItem.js
@@ -3,31 +3,28 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { NavLink, Row, Col } from "reactstrap"
-import { Link } from "react-router-dom"
 
 const NavbarLeftItem = ({ icon, nameLink, link }) => (
   <div>
     <Row>
       <Col xs="12">
-        <Link to={link}>
-          <NavLink
-            href={link}
+        <NavLink
+          href={link}
+          style={{
+            color: "white",
+            opacity: 0.5
+          }}
+        >
+          <img
+            alt=""
+            src={icon}
             style={{
-              color: "white",
-              opacity: 0.5
+              text: "center",
+              opacity: 0.8
             }}
-          >
-            <img
-              alt=""
-              src={icon}
-              style={{
-                text: "center",
-                opacity: 0.8
-              }}
-            />
-            {nameLink}
-          </NavLink>
-        </Link>
+          />
+          {nameLink}
+        </NavLink>
       </Col>
     </Row>
     <hr />
