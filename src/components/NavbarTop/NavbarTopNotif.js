a38167ea07dbeaeae7e7a4e7aff098dc7922044c
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-27 09:50:32 +0200

    test css notif

--- src/components/NavbarTop/NavbarTopNotif.js
+++ src/components/NavbarTop/NavbarTopNotif.js
@@ -2,20 +2,41 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import NavbarTopNotifBubble from "./NavbarTopNotifBubble"
+import { Row, Col } from "reactstrap"
+
+import NotifContainer from "../../containers/NotifContainer"
 
 const NavbarTopNotif = () => {
   return (
-    <div>
-      <img
+    <Row style={{ height: "90px" }}>
+      <Col
+        xs={{ size: 6 }}
         style={{
-          marginTop: "37px"
+          backgroundColor: "none",
+          margin: "auto",
+          paddingRight: "0px"
         }}
-        src={process.env.PUBLIC_URL + "img/icon_notification.png"}
-        alt="iconNotif"
-      />
-      <NavbarTopNotifBubble notifsCount={66} />
-    </div>
+        className=""
+      >
+        <img
+          style={{
+            backgroundColor: "none"
+          }}
+          src={process.env.PUBLIC_URL + "img/icon_notification.png"}
+          alt="iconNotif"
+        />
+      </Col>
+      <Col
+        xs={{ size: 6 }}
+        style={{
+          backgroundColor: "none",
+          marginTop: "19%",
+          marginLeft: "-5%"
+        }}
+      >
+        <NotifContainer />
+      </Col>
+    </Row>
   )
 }
 
