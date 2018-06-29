990b425684418aaa81f17bec425f9e8ef99b94c5
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-06-29 11:38:04 +0200

    display path well

--- src/components/PageFolders/PathElement.js
+++ src/components/PageFolders/PathElement.js
@@ -1,7 +1,16 @@
 import React from "react"
 
 const PathElement = ({ name }) => {
-  return <a>/{name}</a>
+  return (
+    <a>
+      <img
+        style={{ width: "0.2em", margin: "0.4em" }}
+        src={process.env.PUBLIC_URL.concat("Icons/icon_arrow_right.png")}
+        alt="icon right arrow"
+      />
+      {`  ${name} `}
+    </a>
+  )
 }
 
 export default PathElement
