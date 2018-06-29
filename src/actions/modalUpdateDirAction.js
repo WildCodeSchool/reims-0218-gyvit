5044dfd996485d1b58c057f9581040c51f6b36b4
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-29 10:13:21 +0200

    add modalUpdateDirAction

---
+++ src/actions/modalUpdateDirAction.js
@@ -0,0 +1,10 @@
+export const SHOW_MODAL_UPDATE_DIR = "SHOW_MODAL_UPDATE_DIR"
+export const HIDE_MODAL_UPDATE_DIR = "HIDE_MODAL_UPDATE_DIR"
+
+export const makeShowModalUpdateDir = () => ({
+  type: SHOW_MODAL_UPDATE_DIR
+})
+
+export const makeHideModalUpdateDir = () => ({
+  type: HIDE_MODAL_UPDATE_DIR
+})
