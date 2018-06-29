cb5f2be69420ddce6bf0a8406f7998588e744768
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-29 09:37:04 +0200

    container to display firstname of connected user

---
+++ src/containers/DashboardDisplayNameContainer.js
@@ -0,0 +1,47 @@
+import { connect } from "react-redux"
+import React, { Component } from "react"
+import { Col } from "reactstrap"
+
+//map the props profile to the user branch of the state
+const mapStateToProps = state => {
+  return {
+    profile: state.user
+  }
+}
+
+class DashboardDisplayNameContainer extends Component {
+  render() {
+    return (
+      console.log(this.props.profile),
+      (
+        <Col md="12">
+          <p
+            style={{
+              fontSize: "24px",
+              height: "33px",
+              marginTop: "5%",
+              textAlign: "center"
+            }}
+          >
+            Welcome to Gyvit, {this.props.profile.firstname}
+          </p>
+
+          <p
+            style={{
+              fontSize: "18px",
+              height: "24px",
+              textAlign: "center",
+              color: "#000000",
+              opacity: 0.45,
+              marginBottom: "60px"
+            }}
+          >
+            What would you like to do ?
+          </p>
+        </Col>
+      )
+    )
+  }
+}
+
+export default connect(mapStateToProps)(DashboardDisplayNameContainer)
