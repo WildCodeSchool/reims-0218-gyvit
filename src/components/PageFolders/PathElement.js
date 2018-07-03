0ec89b3a20577e9a6e191d8cc3513ee67b56a297
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-07-03 17:27:03 +0200

    pathElement chngaes its style when hover

--- src/components/PageFolders/PathElement.js
+++ src/components/PageFolders/PathElement.js
@@ -1,16 +1,42 @@
 import React from "react"
 
-const PathElement = ({ name, onClick = () => {} }) => {
-  return (
-    <span onClick={onClick}>
-      <img
-        style={{ width: "0.2em", margin: "0.4em" }}
-        src={process.env.PUBLIC_URL.concat("Icons/icon_arrow_right.png")}
-        alt="icon right arrow"
-      />
-      <span>{name}</span>
-    </span>
-  )
+class PathElement extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { style: "" }
+  }
+
+  onMouseEnter(underlined) {
+    if (underlined) this.setState({ style: "underline" })
+  }
+
+  onMouseLeave(underlined) {
+    this.setState({ style: "none" })
+  }
+
+  render() {
+    return (
+      <span
+        onClick={this.props.onClick}
+        onMouseEnter={() => {
+          console.log("Enter, underlined: ", this.props.underlined)
+          this.onMouseEnter(this.props.underlined)
+        }}
+        style={{ textDecoration: this.state.style }}
+        onMouseLeave={() => {
+          console.log("Leave, underlined: ", this.props.underlined)
+          this.onMouseLeave(this.props.underlined)
+        }}
+      >
+        <img
+          style={{ width: "0.2em", margin: "0.5em" }}
+          src={process.env.PUBLIC_URL.concat("Icons/icon_arrow_right.png")}
+          alt="icon right arrow"
+        />
+        <span>{this.props.name}</span>
+      </span>
+    )
+  }
 }
 
 export default PathElement
