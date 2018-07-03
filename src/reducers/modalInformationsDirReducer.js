cae490febfbbf9e8199f4264d97ca285e92ca6ed
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-07-03 13:14:03 +0200

    init modalInformationsDirReducer.js

---
+++ src/reducers/modalInformationsDirReducer.js
@@ -0,0 +1,26 @@
+import {
+  SHOW_MODAL_INFORMATIONS_DIR,
+  HIDE_MODAL_INFORMATIONS_DIR
+} from "../actions/modalInformationsDirAction"
+
+const initialState = {
+  visibilityInformationsDir: false
+}
+
+// initialState is the default value of prevState
+const modalInformationsDirReducer = (prevState = initialState, action) => {
+  if (action.type === SHOW_MODAL_INFORMATIONS_DIR) {
+    return {
+      visibilityInformationsDir: true
+    }
+  }
+  if (action.type === HIDE_MODAL_INFORMATIONS_DIR) {
+    // we modify the visibilityInformationsDir in the store
+    return {
+      visibilityInformationsDir: false
+    }
+  }
+  return prevState
+}
+
+export default modalInformationsDirReducer
