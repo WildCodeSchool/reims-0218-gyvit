379788a2b82a5fcdee5adcd2befa3ad70d5357a4
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-07-04 16:44:44 +0200

    fix style on rowDir

--- src/components/PageFolders/RowDir.js
+++ src/components/PageFolders/RowDir.js
@@ -10,18 +10,11 @@ const RowDir = ({ dir, onDirclick }) => {
       <th onClick={() => onDirclick(_id)} style={{ width: "40%" }}>
         <img
           style={{
-            width: "45%",
-            height: "9%",
-            fontFamily: "DepotNew",
-            fontSize: "14px",
-            marginTop: "14%",
-            fontWeight: "normal",
-            fontStyle: "normal",
-            fontStretch: "normal",
-            lineHeight: "normal",
-            letterSpacing: "normal",
-            textAlign: "left",
-            color: "#a5a0c2"
+            width: "30px",
+            height: "29px",
+            marginTop: "20px",
+            marginBottom: "20px",
+            marginRight: "19.1px"
           }}
           src={process.env.PUBLIC_URL + "Icons/icon_folder.png"}
           alt="Directory Icon"
