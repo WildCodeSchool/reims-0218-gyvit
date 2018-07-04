9116e83c1104bbd0bf66ffc8e82a6d47fe488148
Pierre <dumaypierre@outlook.fr>
2018-07-04 13:35:14 +0200

    add cursor on arianne

--- src/components/PageFolders/PathElement.js
+++ src/components/PageFolders/PathElement.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const PathElement = ({ name, onClick = () => {} }) => {
   return (
-    <span onClick={onClick}>
+    <span style={{ cursor: "pointer" }} onClick={onClick}>
       <img
         style={{ width: "0.2em", margin: "0.4em" }}
         src={process.env.PUBLIC_URL.concat("Icons/icon_arrow_right.png")}
