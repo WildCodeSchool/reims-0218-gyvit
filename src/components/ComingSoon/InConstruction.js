5ebe628e5e91a59c95652ff8aea0ad3ebf850e53
Thib <thibaud.royer051@gmail.com>
2018-07-05 10:06:40 +0200

    commit on develop strange...

--- src/components/ComingSoon/InConstruction.js
+++ src/components/ComingSoon/InConstruction.js
@@ -1,30 +1,9 @@
 import React from "react"
 import { Container } from "reactstrap"
-import logo from "./logo.gif"
-import "../../App.css"
 
 const ComingSoon = () => {
   return (
     <Container>
-      <img
-        style={{
-          marginLeft: "35%",
-          marginTop: "12%"
-        }}
-        src={logo}
-        alt="meduza icon"
-        className="meduza-logo"
-      />
-      <p
-        style={{
-          color: "#231b56",
-          fontSize: "40px",
-          fontStyle: "gras",
-          marginLeft: "35%"
-        }}
-      >
-        Coming Soon !!
-      </p>
       <img
         style={{
           marginLeft: "35%",
