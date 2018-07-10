0d580920aa8247c8eae5841aa6aa1c571b779ff2
Anthony <elmorodjeur@gmail.com>
2018-07-10 11:33:08 +0200

    change name of actions in sortActions

--- src/actions/sortActions.js
+++ src/actions/sortActions.js
@@ -1,7 +1,7 @@
 export const SORT_DIRS_BY_NAME_ASC = "SORT_DIRS_BY_NAME_ASC"
 export const SORT_DIRS_BY_NAME_DESC = "SORT_DIRS_BY_NAME_DESC "
-export const SORT_DIRS_BY_LAST_UPDATE_ASC = "SORT_DIRS_BY_LAST_UPDATE_ASC"
-export const SORT_DIRS_BY_LAST_UPDATE_DESC = "SORT_DIRS_BY_LAST_UPDATE_DESC"
+export const SORT_DIRS_BY_DATE_ASC = "SORT_DIRS_BY_DATE_ASC"
+export const SORT_DIRS_BY_DATE_DESC = "SORT_DIRS_BY_DATE_DESC"
 
 
 export const makeSortDirsByNameAsc = () =>({
@@ -14,10 +14,12 @@ export const makeSortDirsByNameDesc = () => ({
 
 })
 
-export const makeSortDirsByLastUpdateAsc = () => ({
-  type: SORT_DIRS_BY_LAST_UPDATE_ASC,
+export const makeSortDirsByDateAsc = () => ({
+  type: SORT_DIRS_BY_DATE_ASC,
+
 })
 
-export const makeSortDirsByLastUpdateDesc = () => ({
-  type: SORT_DIRS_BY_LAST_UPDATE_DESC,
+export const makeSortDirsByDateDesc = () => ({
+  type: SORT_DIRS_BY_DATE_DESC,
+
 })
