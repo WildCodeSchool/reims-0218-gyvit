c3362ab7e7ae868033cab8558dc8b40e2abcf48e
Unknown <tarteserviceinformatique@sfr.fr>
2018-07-11 16:10:23 +0200

    retrieveDir with axios

--- src/api/directorys/retrieveDirectorys.js
+++ src/api/directorys/retrieveDirectorys.js
@@ -1,12 +1,20 @@
 import { getToken } from "../users/localStorageToken"
+const axios = require("axios")
+const root = "dir_k8G3aBS8TSWv9zEDj5ao"
 
-export const retrieveDir = (_id = "dir_k8G3aBS8TSWv9zEDj5ao") =>
-  fetch(`https://dev.gyvit.io/api/storage/directorys/${_id}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "http://localhost:3000",
-      Authorization: `Bearer ${getToken()}`
-    }
-  }).then(res => res.json())
+const config = {
+  baseURL: "https://dev.gyvit.io/api/",
+  method: "get",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    Authorization: `Bearer ${getToken()}`
+  }
+}
+
+export const retrieveDir = (_id = root) =>
+  axios({
+    ...config,
+    url: `storage/directorys/list?name=${_id}`
+  }).then(res => res.data)
