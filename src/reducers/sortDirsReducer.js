3a700f21f7eedcb9f54aec208f912a4301f28f55
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-07-12 17:31:24 +0200

    resolve

--- src/reducers/sortDirsReducer.js
+++ src/reducers/sortDirsReducer.js
@@ -1,52 +1,17 @@
-import  {SORT_DIRS_BY_NAME_ASC,
-  SORT_DIRS_BY_NAME_DESC,
-  SORT_DIRS_BY_DATE_ASC,
-  SORT_DIRS_BY_DATE_DESC} from "../actions/sortDirsActions"
-
+import { RETRIEVE_DIR_SUCCESS } from "../actions/foldersActions"
 
 const initialState = []
 
-const compareDirs = (prevState = initialState, action) => {
-  if (action.type === SORT_DIRS_BY_NAME_ASC){
-    return prevState.slice().sort(function(a, b) {
-      var nameA = a.name.toLowerCase(),
-        nameB = b.name.toLowerCase()
-      if (nameA < nameB)
-        return -1
-      if (nameA > nameB)
-        return 1
-      return 0
-    }
-  if (action.type ===SORT_DIRS_BY_NAME_DESC){
-    return ;
+const sortDirsReducer = (prevState = initialState, action) => {
+  if (action.type === RETRIEVE_DIR_SUCCESS) {
+    const array = action.response.dirs
+    const sortedArray = array.sort((a, b) => {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
+    })
+    return sortedArray
   }
-  if (action.type === SORT_DIRS_BY_DATE_ASC){
-    return ;
-  }
-  if (action.type === SORT_DIRS_BY_DATE_DESC){
-    return ;
-  }
-  }
-
-
-
-
-// const compareDirs = (a, b,direction) => {
-// // we don't change the order
-// if (((direction==="asc") && (a < b)) || ((direction==="desc") && (a > b)))
-// return -1;
-// // we change the order
-// if (((direction==="asc") && (a > b)) || ((direction==="desc") && (a < b)))
-// return 1;
-// // a doit être égal à b
-// return 0;
-// }
-
-
-// const sortDirsReducer = (prevState = initialState, action)=> {
-// if (action.type===SORT_DIRS_BY_NAME_ASC || action.type===SORT_DIRS_BY_NAME_DESC)
-
-// return prevState.sort(action.name) => compareDirs(a,b, action.action)
-// }
-
-export default compareDirs
+  return prevState
+}
+export default sortDirsReducer
