6773818308d54035d7ed6c7be7ac42654aea3221
Unknown <tarteserviceinformatique@sfr.fr>
2018-07-12 09:24:59 +0200

    dleeteFile with axios

--- src/api/files/deleteFile.js
+++ src/api/files/deleteFile.js
@@ -1,12 +1,20 @@
 import { getToken } from "../users/localStorageToken"
+const axios = require("axios")
 
-export const deleteFile = (fileId, fileName) =>
-  fetch(`https://dev.gyvit.io/api/storage/files/${fileId}`, {
-    method: "DELETE",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    },
-    body: JSON.stringify({ name: fileName })
-  }).then(res => res.json())
+const config = {
+  baseURL: "https://dev.gyvit.io/api/",
+  method: "delete",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${getToken()}`
+  }
+}
+
+export const deleteFile = (fileId, fileName) => {
+  return axios({
+    ...config,
+    url: `storage/files/${fileId}`,
+    data: JSON.stringify({ name: fileName })
+  }).then(res => res.data)
+}
