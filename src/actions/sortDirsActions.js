1d7ec5eec499d926e4d93ca4af998211f5702334
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-07-13 11:24:07 +0200

    sortDirsReducer done

--- src/actions/sortDirsActions.js
+++ src/actions/sortDirsActions.js
@@ -3,19 +3,18 @@ export const SORT_DIRS_BY_NAME_DESC = "SORT_DIRS_BY_NAME_DESC "
 export const SORT_DIRS_BY_DATE_ASC = "SORT_DIRS_BY_DATE_ASC"
 export const SORT_DIRS_BY_DATE_DESC = "SORT_DIRS_BY_DATE_DESC"
 
-
-export const makeSortDirsByNameAsc = () =>({
-  type: SORT_DIRS_BY_NAME_ASC, 
+export const makeSortDirsByNameAsc = () => ({
+  type: SORT_DIRS_BY_NAME_ASC
 })
 
 export const makeSortDirsByNameDesc = () => ({
-  type: SORT_DIRS_BY_NAME_DESC, 
+  type: SORT_DIRS_BY_NAME_DESC
 })
 
 export const makeSortDirsByDateAsc = () => ({
-  type: SORT_DIRS_BY_DATE_ASC, 
+  type: SORT_DIRS_BY_DATE_ASC
 })
 
 export const makeSortDirsByDateDesc = () => ({
-  type: SORT_DIRS_BY_DATE_DESC, 
+  type: SORT_DIRS_BY_DATE_DESC
 })
