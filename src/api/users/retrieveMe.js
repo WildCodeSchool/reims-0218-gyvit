348e9c0914afa6ee8d880933a98ba91754a48ee3
Unknown <tarteserviceinformatique@sfr.fr>
2018-07-16 15:28:47 +0200

    fix error of login

--- src/api/users/retrieveMe.js
+++ src/api/users/retrieveMe.js
@@ -1,23 +1,22 @@
 import { getToken } from "./localStorageToken"
 const axios = require("axios")
 
-const config = {
-  baseURL: `${process.env.REACT_APP_BASE_URL}`,
-  url: `user/me`,
-  method: "get",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${getToken()}`
-  }
-}
-
 /**
  *
  * @returns {{object:"account",_id:{string},mail:{string},firstname:{string},lastname:{string},role:{string},active:{number},created:{string},modified:{string}}}: acct object for user
  */
 
 export const retrieveMe = () => {
+  const config = {
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    url: `user/me`,
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`
+    }
+  }
   console.log("token AVANT axios du retrieveMe: ", getToken())
   return axios(config)
     .then(res => {
