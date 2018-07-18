bfa28b5596e0488e19049ff868cfeb06a95d4eae
Pierre <dumaypierre@outlook.fr>
2018-07-18 11:30:24 +0200

    clean code

--- src/actions/modalCreateDirAction.test.js
+++ src/actions/modalCreateDirAction.test.js
@@ -7,18 +7,16 @@ import {
 
 describe("action show modal create dir", () => {
   it("should return a type SHOW_MODAL_CREATE_DIR  in state", () => {
-    
     const expected = {
       type: SHOW_MODAL_CREATE_DIR
     }
-    
+
     expect(makeShowModalCreateDir()).toEqual(expected)
   })
 })
 
 describe("action hide modal create dir", () => {
   it("should return a type HIDE_MODAL_CREATE_DIR  in state", () => {
-    
     const expected = {
       type: HIDE_MODAL_CREATE_DIR
     }
