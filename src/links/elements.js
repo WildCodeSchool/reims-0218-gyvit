5ddbeacdf9dfd873f7dbfc730c4d9ba1130de0b0
Pierre <dumaypierre@outlook.fr>
2018-07-23 16:03:17 +0200

    text card

--- src/links/elements.js
+++ src/links/elements.js
@@ -4,7 +4,7 @@ export const elements = [
     image: "/img/request_document.png",
     titre: "Demande de documents",
     bio:
-      "Lancez vous dans la demande de documents. Dès maintenant faites vos demande de fichiers.",
+      "Lancez-vous dans la demande de documents. Demandez et gérez vos documents en toute sérénité à qui vous voulez où que vous soyez.",
     buttonText: "Demande de documents",
     link: "/requests"
   },
@@ -13,7 +13,7 @@ export const elements = [
     image: "/img/document_share.png",
     titre: "Partage de documents",
     bio:
-      "Lancez vous dans le partage de documents. Partager vos fichiers en toute sérénité à qui vous voulez où que vous soyez.",
+      "Lancez-vous dans le partage de documents. Partager et gérez les droits en toute sérénité à qui vous voulez où que vous soyez.",
     buttonText: "Partage de documents",
     link: "/shares"
   },
@@ -22,7 +22,7 @@ export const elements = [
     image: "/img/files_store.png",
     titre: "Mes documents",
     bio:
-      "Lancez vous dans le stockage de vos documents. Télécharger et sauvegarder tous vos fichiers et documents quand vous voulez où que vous soyez !",
+      "Lancez-vous dans le stockage de documents. Gérez les droits de vos documents et en toute sérénité où que vous soyez.",
     buttonText: "Télécharger des documents",
     link: "/files"
   }
