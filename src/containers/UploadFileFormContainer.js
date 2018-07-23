4eed3cd9363063eb15b3c238cb83573f24878e5d
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-07-23 15:00:16 +0200

    upload test

--- src/containers/UploadFileFormContainer.js
+++ src/containers/UploadFileFormContainer.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { Col, Button, Form, FormGroup, Label, CustomInput } from "reactstrap"
+import { Col, Button, Form, FormGroup, Label } from "reactstrap"
 
+import { DateTime } from "luxon"
 import ModalErrorContainer from "./ModalErrorContainer"
 import { uploadFile } from "../api/files/uploadFile"
 import { makeAddAFileSuccess } from "../actions/filesActions"
 import { makeHideModalCreateFile } from "../actions/modalCreateFileAction"
 import { makeShowModalError } from "../actions/errorsActions"
+import { convertDateFromJsonToFrench } from "../functions/dirs"
 
 const mapStateToProps = state => ({
   destination: state.currentDir._id
@@ -21,41 +23,67 @@ const mapDispatchToProps = dispatch => ({
 class UploadFileFormContainer extends Component {
   constructor(props) {
     super(props)
-    this.state = { visibilityError: false }
+    const CurrentTime = DateTime.fromMillis(Date.now())
+      .setLocale("en-US")
+      .toLocaleString()
+    this.state = {
+      visibilityError: false,
 
-    this.onHandleFileUpload = this.onHandleFileUpload.bind(this)
+      file: {
+        _id: "file_odd9OMeNdsds2lE3ePeN02N9",
+        object: "file",
+        name: "Sksdfdsfetch",
+        size: "1000",
+        ext: "php",
+        type: "",
+        remove: 0,
+        created: "2018-03-29T00:00:00+00:00",
+        modified: `${CurrentTime}`,
+        removed: null
+      }
+    }
+
+    // this.onHandleFileUpload = this.onHandleFileUpload.bind(this)
   }
 
-  onHandleFileUpload() {
-    const data = new FormData()
-    data.append("file", this.fileInput.files[0])
-    data.append("destination", this.props.destination)
-    data.append("name", this.fileInput.files[0].name)
-    //build data
-    uploadFile(data)
-      .then(response => this.props.onFileUpload(response))
-      .catch(response => this.props.onError(response.message))
-    this.props.onHideModal()
-  }
+  // onHandleFileUpload() {
+  //   const data = new FormData()
+  //   data.append("file", this.fileInput.files[0])
+  //   data.append("destination", this.props.destination)
+  //   data.append("name", this.fileInput.files[0].name)
+  //   //build data
+  //   uploadFile(data)
+  //     .then(response => this.props.onFileUpload(response))
+  //     .catch(response => this.props.onError(response.message))
+  //   this.props.onHideModal()
+  // }
 
   render() {
     return (
       <Form>
         <ModalErrorContainer />
         <FormGroup row>
+          <Label for="inputFile" sm={2}>
+            File
+          </Label>
           <Col sm={10}>
-            <Label for="exampleCustomFileBrowser">File:</Label>
-            <CustomInput
+            <input
               type="file"
-              id="exampleCustomFileBrowser"
-              name="customFile"
+              name="file"
+              id="inputFile"
+              ref={input => {
+                this.fileInput = input
+              }}
             />
           </Col>
         </FormGroup>
 
         <FormGroup row>
           <Col sm={{ size: 10, offset: 2 }}>
-            <Button type="button" onClick={this.onHandleFileUpload}>
+            <Button
+              type="button"
+              onClick={() => this.props.onFileUpload(this.state.file)}
+            >
               Submit
             </Button>
           </Col>
@@ -65,7 +93,6 @@ class UploadFileFormContainer extends Component {
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UploadFileFormContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  UploadFileFormContainer
+)
